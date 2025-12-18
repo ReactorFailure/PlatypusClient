8f218f1778f06366f53b8d42d61ec592761744e0
ReactorFailure <soup2soup@proton.me>
2025-12-18 17:48:32 -0500

    Initial commit

---
+++ src/main/java/net/reactorfailure/platypusclient/ClientSide.java
@@ -0,0 +1,10 @@
+package net.reactorfailure.platypusclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientSide implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/reactorfailure/platypusclient/PlatypusClientMain.java
@@ -0,0 +1,16 @@
+package net.reactorfailure.platypusclient;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PlatypusClientMain implements ModInitializer {
+	public static final String MOD_ID = "platypusclient";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
