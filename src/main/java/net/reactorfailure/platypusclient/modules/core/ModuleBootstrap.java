6831d2ede7fd708c6c33d772f06fae1654fefe86
ReactorFailure <soup2soup@proton.me>
2026-01-08 19:35:07 -0500

    Made ModuleBootstrap more nicer somewhat

--- src/main/java/net/reactorfailure/platypusclient/modules/core/ModuleBootstrap.java
+++ src/main/java/net/reactorfailure/platypusclient/modules/core/ModuleBootstrap.java
@@ -1,11 +1,20 @@
 package net.reactorfailure.platypusclient.modules.core;
 
-import net.reactorfailure.platypusclient.modules.NightVisionModule;
-import net.reactorfailure.platypusclient.modules.PersistentSneakModule;
+import net.reactorfailure.platypusclient.modules.*;
 
 public final class ModuleBootstrap {
     public static void init() {
-        ModuleManager.register(new NightVisionModule());
-        ModuleManager.register(new PersistentSneakModule());
+        registerModules(
+                new NightVisionModule(),
+                new PersistentSneakModule(),
+                new PersistentRunModule(),
+                new InstantRespawnModule()
+        );
+    }
+
+    private static void registerModules(Module ... modules) {
+        for (Module module : modules) {
+            ModuleManager.register(module);
+        }
     }
 }
